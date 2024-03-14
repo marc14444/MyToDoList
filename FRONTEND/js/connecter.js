2a59f8467f4054a6e1d79440cfdb860d62a031e9
marc14444 <108020062+marc14444@users.noreply.github.com>
2024-03-14 13:34:52 +0000

    Update connecter.js

--- FRONTEND/js/connecter.js
+++ FRONTEND/js/connecter.js
@@ -1,12 +1,12 @@
 const session = localStorage.getItem('session');
 
-let nom = document.querySelector('.afficherNom');
-nom.innerHTML = `bonjour et bienvenue`
+//let nom = document.querySelector('.afficherNom');
+//nom.innerHTML = `bonjour et bienvenue`
 
 if(session === null){
     window.location.href = './connexion.html'
 }
-let deconnexion = document.querySelector('.deconnexion');
+let deconnexion = document.querySelector('#deconnexion');
 deconnexion.addEventListener('click', (e)=>{
     e.preventDefault();
     localStorage.removeItem('session');
